7daae1770db0142df17b929de58c1f345e4246a5
imskanand <the.anand3099@gmail.com>
2023-07-31 15:01:42 +0530

    Movies styling done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,65 @@
-const page= () => {
-  return (<div>
-    <div className="flex-col">
-      Sign IN
-    </div>
-  </div>)
-}
+"use client";
 
-export default page
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+    router.refresh();
+  };
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    router.refresh();
+  };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col p-6 rounded-xl bg-gray-700 gap-5 text-black">
+        <input
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email"
+          className="p-3 rounded-xl"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Password"
+          className="p-3 rounded-xl"
+        />
+        <button className="rounded p-2 bg-slate-600" onClick={handleSignUp}>
+          Sign up
+        </button>
+        <button className="rounded p-2 bg-gray-500 " onClick={handleSignIn}>
+          Sign in
+        </button>
+        <button onClick={handleSignOut}>Sign out</button>
+      </div>
+    </div>
+  );
+}
--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useQuery } from "react-query";
+import Image from "next/image";
 
 const options = {
   method: "GET",
@@ -40,13 +41,16 @@ interface Result {
   vote_count: number;
 }
 
+const template: string = "https://image.tmdb.org/t/p/w500";
+
 const Home = () => {
   const { data, isLoading } = useQuery<UpcomingMovies>({
     queryKey: ["movies"],
     queryFn: () =>
-      fetch("https://api.themoviedb.org/3/movie/upcoming", options).then(
-        (res) => res.json()
-      ),
+      fetch(
+        "https://api.themoviedb.org/3/movie/upcoming?language=en&region=us",
+        options
+      ).then((res) => res.json()),
   });
 
   if (isLoading) {
@@ -59,15 +63,32 @@ const Home = () => {
 
   const results: Result[] = data.results;
   return (
-    <div className="flex-col p-5">
+    <>
+    <p className="flex justify-center items-center text-3xl font-bold mt-10">Movies</p>
+    <div className="grid grid-cols-4 mt-10 gap-10 mx-20">
       {results.map((result) => (
-        <p className="font-bold" key={result.id}>
-          {result.title}
-          {result.overview}
-          {result.video}
-        </p>
+        <div
+          className="flex-col block shadow-lg shadow-gray-700 hover:shadow-green-200 rounded-xl font-bold p-2 mb-10 mr-4 scale-75"
+          key={result.id}
+        >
+          <Image
+            src={`${template}${result.poster_path}`}
+            alt="Movie Poster"
+            width={180}
+            height={270}
+            className="w-full"
+          />
+          <p className="truncate my-1 text-2xl">{result.title}</p>
+          <p className="text-xl font-semibold text-gray-100">
+            Popularity : {result.popularity.toFixed(0)}
+          </p>
+          <p className="text-xl text-gray-500 font-medium">
+            {result.release_date.toString()}
+          </p>
+        </div>
       ))}
     </div>
+    </>
   );
 };
 
